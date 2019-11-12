0200238d32df46e5f019f6316393c481f14f600b
joaopauloschuler <43456488+joaopauloschuler@users.noreply.github.com>
2019-11-12 16:53:09 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+long_description = '''
+K-CAI NEURAL API is a Keras based neural network API that will allow you to prototype faster!
+This project is a subproject from a bigger and older project called CAI and is sister to the pascal based CAI NEURAL API.
+'''
+
+setup(name='cai',
+      version='0.0.1',
+      description='K-CAI NEURAL API',
+      long_description=long_description,
+      author='Joao Paulo Schwarz Schuler',
+      url='https://github.com/joaopauloschuler/k-neural-api',
+      install_requires=['Keras>=2.2.5',
+                        'pandas>=0.22.0',
+                        'scikit-image>=0.15.0',
+                        'numpy']
+      },
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=['cai'])
