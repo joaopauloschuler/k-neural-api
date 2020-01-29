5baa70dcec5827befd3ea7b80f4687196418f480
joaopauloschuler <jp@schulers.com>
2020-01-29 19:19:43 +0100

    Coding plant leaf model.

--- cai/models.py
+++ cai/models.py
@@ -4,23 +4,26 @@ from keras.models import Model
 #import cai.datasets
 #import cai.layers
 
-def plant_leaf(pinput_shape, num_classes,  l2_decay=0.0, dropout_drop_rate=0):
+def plant_leaf(pinput_shape, num_classes,  l2_decay=0.0, dropout_drop_rate=0, has_batch_norm=True):
     img_input = keras.layers.Input(shape=pinput_shape)
     last_tensor = keras.layers.Conv2D(32, (3, 3), padding='valid',
         input_shape=pinput_shape, 
         kernel_regularizer=keras.regularizers.l2(l2_decay))(img_input)
+    if (has_batch_norm): last_tensor = keras.layers.BatchNormalization(axis=3, epsilon=1.001e-5)(last_tensor)
     last_tensor = keras.layers.Activation('relu')(last_tensor)
     last_tensor = keras.layers.MaxPooling2D(2, strides=2)(last_tensor)
     
     last_tensor = keras.layers.Conv2D(16, (3, 3), padding='valid',
         input_shape=pinput_shape, 
         kernel_regularizer=keras.regularizers.l2(l2_decay))(last_tensor)
+    if (has_batch_norm): last_tensor = keras.layers.BatchNormalization(axis=3, epsilon=1.001e-5)(last_tensor)
     last_tensor = keras.layers.Activation('relu')(last_tensor)
     last_tensor = keras.layers.MaxPooling2D(2, strides=2)(last_tensor)
     
     last_tensor = keras.layers.Conv2D(16, (3, 3), padding='valid',
         input_shape=pinput_shape, 
         kernel_regularizer=keras.regularizers.l2(l2_decay))(last_tensor)
+    if (has_batch_norm): last_tensor = keras.layers.BatchNormalization(axis=3, epsilon=1.001e-5)(last_tensor)
     last_tensor = keras.layers.Activation('relu')(last_tensor)
     
     if (dropout_drop_rate > 0.0):
