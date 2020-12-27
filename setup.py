9e11a4f5ed1bb33c780517341b725b1cdcd784b7
joaopauloschuler <jp@schulers.com>
2020-12-27 15:12:42 +0100

    New version number.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ This project is a subproject from a bigger and older project called CAI and is s
 '''
 
 setup(name='cai',
-      version='0.1.0',
+      version='0.1.1',
       description='K-CAI NEURAL API',
       long_description=long_description,
       author='Joao Paulo Schwarz Schuler',
