6a6d5e7d034a9bd23f1f028ce1179598ca80c25b
joaopauloschuler <jp@schulers.com>
2021-01-07 02:57:00 +0100

    Adding create_image_generator_no_augmentation.

--- cai/util.py
+++ cai/util.py
@@ -351,6 +351,39 @@ def create_image_generator(
         # fraction of images reserved for validation (strictly between 0 and 1)
         validation_split=validation_split)
 
+def create_image_generator_no_augmentation():
+    """This is a wrapper for keras.preprocessing.image without data augmentation.
+    """
+    return create_image_generator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        zca_epsilon=0,  # epsilon for ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        # randomly shift images horizontally (fraction of total width)
+        width_shift_range=0.0,
+        # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.0,
+        shear_range=0.0,  # set range for random shear
+        zoom_range=0.0,  # set range for random zoom
+        channel_shift_range=0.,  # set range for random channel shifts
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        cval=0.,  # value used for fill_mode = "constant"
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0
+    )
+
 def relu(adata):   
     """Calculates the rectifier linear unit.
     # Arguments
