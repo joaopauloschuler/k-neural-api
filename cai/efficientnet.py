20cf265dad8e477251432b81f349adb0778a797b
joaopauloschuler <jp@schulers.com>
2021-05-06 01:05:49 +0200

    Adapting EfficientNet.

--- cai/efficientnet.py
+++ cai/efficientnet.py
@@ -46,11 +46,14 @@ from __future__ import division
 from __future__ import print_function
 
 import math
+from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import backend
 from tensorflow.keras import models
 from tensorflow.keras import utils
 from tensorflow.keras.applications import imagenet_utils
+import cai.util
+import cai.models
 
 def correct_pad(backend, inputs, kernel_size):
     """Returns a tuple for zero-padding for 2D convolution with downsampling.
@@ -219,9 +222,7 @@ def block(inputs, activation_fn=swish, drop_rate=0., name='',
                                noise_shape=(None, 1, 1, 1),
                                name=name + 'drop')(x)
         x = layers.add([x, inputs], name=name + 'add')
-
     return x
-
 
 def EfficientNet(width_coefficient,
                  depth_coefficient,
@@ -370,6 +371,286 @@ def EfficientNet(width_coefficient,
 
     return model
 
+def kPointwiseConv2DType0(last_tensor,  filters=32,  channel_axis=3,  name=None, activation=None, has_batch_norm=True, use_bias=True):
+    return cai.models.conv2d_bn(last_tensor, filters, 1, 1, name=name, activation=activation, has_batch_norm=has_batch_norm, use_bias=use_bias)
+
+def kPointwiseConv2DType1(last_tensor,  filters=32,  channel_axis=3,  name=None, activation=None, has_batch_norm=True, use_bias=True):
+    output_tensor = last_tensor
+    prev_layer_channel_count = keras.backend.int_shape(last_tensor)[channel_axis]
+    channel_count = filters
+    if (prev_layer_channel_count % 2 == 0) and (channel_count % 2 > 0):
+        channel_count = channel_count + 1
+    group_count = 0
+    if (prev_layer_channel_count > 64):
+        group_count = cai.util.get_max_acceptable_common_divisor(prev_layer_channel_count, channel_count, max_acceptable = (prev_layer_channel_count//32) )
+        output_tensor = cai.models.conv2d_bn(output_tensor, channel_count, 1, 1, name=name, activation=activation, has_batch_norm=has_batch_norm, groups=group_count, use_bias=use_bias)
+    else:
+        output_tensor = cai.models.conv2d_bn(output_tensor, channel_count, 1, 1, name=name, activation=activation, has_batch_norm=has_batch_norm, use_bias=use_bias)        
+    return output_tensor
+
+def kPointwiseConv2DType2(last_tensor,  filters=32, channel_axis=3, name=None, activation=None, has_batch_norm=True, use_bias=True):
+    output_tensor = last_tensor
+    prev_layer_channel_count = keras.backend.int_shape(last_tensor)[channel_axis]
+    channel_count = filters
+    if (prev_layer_channel_count % 2 == 0) and (channel_count % 2 > 0):
+        channel_count = channel_count + 1
+    group_count = 0
+    if (prev_layer_channel_count > 64):
+        group_count = cai.util.get_max_acceptable_common_divisor(prev_layer_channel_count, channel_count, max_acceptable = (prev_layer_channel_count//32) )
+        output_tensor = cai.models.conv2d_bn(output_tensor, channel_count, 1, 1, name=name, activation=activation, has_batch_norm=has_batch_norm, groups=group_count, use_bias=use_bias)
+        compression_tensor = output_tensor
+        output_group_size = channel_count // group_count
+        # output_tensor = keras.layers.BatchNormalization(axis=channel_axis, scale=False, name=name+'_group_bn')(output_tensor)
+        output_tensor = cai.layers.InterleaveChannels(output_group_size, name=name+'_group_interleaved')(output_tensor)
+        output_tensor = cai.models.conv2d_bn(output_tensor, channel_count, 1, 1, name=name+'_group_interconn', activation=activation, has_batch_norm=has_batch_norm, groups=group_count, use_bias=use_bias)
+        output_tensor = keras.layers.add([output_tensor, compression_tensor], name=name+'_inter_group_add')
+    else:
+        output_tensor = cai.models.conv2d_bn(output_tensor, channel_count, 1, 1, name=name, activation=activation, has_batch_norm=has_batch_norm, use_bias=use_bias)        
+    return output_tensor
+
+def kPointwiseConv2D(last_tensor,  filters=32, channel_axis=3,  name=None, activation=None, has_batch_norm=True, use_bias=True, kType=1):
+    if kType == 0:
+        return kPointwiseConv2DType0(last_tensor, filters=filters, channel_axis=channel_axis, name=name, activation=activation, has_batch_norm=has_batch_norm, use_bias=use_bias)
+    if kType == 1:
+        return kPointwiseConv2DType1(last_tensor, filters=filters, channel_axis=channel_axis, name=name, activation=activation, has_batch_norm=has_batch_norm, use_bias=use_bias)
+    if kType == 2:
+        return kPointwiseConv2DType2(last_tensor, filters=filters, channel_axis=channel_axis, name=name, activation=activation, has_batch_norm=has_batch_norm, use_bias=use_bias)
+        
+def kblock(inputs, activation_fn=swish, drop_rate=0., name='',
+          filters_in=32, filters_out=16, kernel_size=3, strides=1,
+          expand_ratio=1, se_ratio=0., id_skip=True, kType=1):
+    """A mobile inverted residual block.
+    # Arguments
+        inputs: input tensor.
+        activation_fn: activation function.
+        drop_rate: float between 0 and 1, fraction of the input units to drop.
+        name: string, block label.
+        filters_in: integer, the number of input filters.
+        filters_out: integer, the number of output filters.
+        kernel_size: integer, the dimension of the convolution window.
+        strides: integer, the stride of the convolution.
+        expand_ratio: integer, scaling coefficient for the input filters.
+        se_ratio: float between 0 and 1, fraction to squeeze the input filters.
+        id_skip: boolean.
+    # Returns
+        output tensor for the block.
+    """
+    bn_axis = 3
+
+    # Expansion phase
+    filters = filters_in * expand_ratio
+    if expand_ratio != 1:
+        #x = layers.Conv2D(filters, 1,
+        #                 padding='same',
+        #                  use_bias=False,
+        #                  kernel_initializer=CONV_KERNEL_INITIALIZER,
+        #                  name=name + 'expand_conv')(inputs)
+        #x = layers.BatchNormalization(axis=bn_axis, name=name + 'expand_bn')(x)
+        #x = layers.Activation(activation_fn, name=name + 'expand_activation')(x)
+        x = kPointwiseConv2D(last_tensor=inputs,  filters=filters, channel_axis=bn_axis,  name=name, activation=activation_fn, has_batch_norm=True, use_bias=False, kType=kType)
+    else:
+        x = inputs
+
+    # Depthwise Convolution
+    if strides == 2:
+        x = layers.ZeroPadding2D(padding=correct_pad(backend, x, kernel_size),
+                                 name=name + 'dwconv_pad')(x)
+        conv_pad = 'valid'
+    else:
+        conv_pad = 'same'
+    x = layers.DepthwiseConv2D(kernel_size,
+                               strides=strides,
+                               padding=conv_pad,
+                               use_bias=False,
+                               depthwise_initializer=CONV_KERNEL_INITIALIZER,
+                               name=name + 'dwconv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + 'bn')(x)
+    x = layers.Activation(activation_fn, name=name + 'activation')(x)
+
+    # Squeeze and Excitation phase
+    if 0 < se_ratio <= 1:
+        filters_se = max(1, int(filters_in * se_ratio))
+        se = layers.GlobalAveragePooling2D(name=name + 'se_squeeze')(x)
+        if bn_axis == 1:
+            se = layers.Reshape((filters, 1, 1), name=name + 'se_reshape')(se)
+        else:
+            se = layers.Reshape((1, 1, filters), name=name + 'se_reshape')(se)
+        se = layers.Conv2D(filters_se, 1,
+                           padding='same',
+                           activation=activation_fn,
+                           kernel_initializer=CONV_KERNEL_INITIALIZER,
+                           name=name + 'se_reduce')(se)
+        se = layers.Conv2D(filters, 1,
+                           padding='same',
+                           activation='sigmoid',
+                           kernel_initializer=CONV_KERNEL_INITIALIZER,
+                           name=name + 'se_expand')(se)
+        x = layers.multiply([x, se], name=name + 'se_excite')
+
+    # Output phase
+    x = layers.Conv2D(filters_out, 1,
+                      padding='same',
+                      use_bias=False,
+                      kernel_initializer=CONV_KERNEL_INITIALIZER,
+                      name=name + 'project_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name=name + 'project_bn')(x)
+    if (id_skip is True and strides == 1 and filters_in == filters_out):
+        if drop_rate > 0:
+            x = layers.Dropout(drop_rate,
+                               noise_shape=(None, 1, 1, 1),
+                               name=name + 'drop')(x)
+        x = layers.add([x, inputs], name=name + 'add')
+    return x
+
+def kEfficientNet(width_coefficient,
+                 depth_coefficient,
+                 default_size,
+                 dropout_rate=0.2,
+                 drop_connect_rate=0.2,
+                 depth_divisor=8,
+                 activation_fn=swish,
+                 blocks_args=DEFAULT_BLOCKS_ARGS,
+                 model_name='efficientnet',
+                 include_top=True,
+                 input_tensor=None,
+                 input_shape=None,
+                 pooling=None,
+                 classes=1000,
+                 kType=1, 
+                 **kwargs):
+    """Instantiates the EfficientNet architecture using given scaling coefficients.
+    Optionally loads weights pre-trained on ImageNet.
+    Note that the data format convention used by the model is
+    the one specified in your Keras config at `~/.keras/keras.json`.
+    # Arguments
+        width_coefficient: float, scaling coefficient for network width.
+        depth_coefficient: float, scaling coefficient for network depth.
+        default_size: integer, default input image size.
+        dropout_rate: float, dropout rate before final classifier layer.
+        drop_connect_rate: float, dropout rate at skip connections.
+        depth_divisor: integer, a unit of network width.
+        activation_fn: activation function.
+        blocks_args: list of dicts, parameters to construct block modules.
+        model_name: string, model name.
+        include_top: whether to include the fully-connected
+            layer at the top of the network.
+        input_tensor: optional Keras tensor
+            (i.e. output of `layers.Input()`)
+            to use as image input for the model.
+        input_shape: optional shape tuple, only to be specified
+            if `include_top` is False.
+            It should have exactly 3 inputs channels.
+        pooling: optional pooling mode for feature extraction
+            when `include_top` is `False`.
+            - `None` means that the output of the model will be
+                the 4D tensor output of the
+                last convolutional layer.
+            - `avg` means that global average pooling
+                will be applied to the output of the
+                last convolutional layer, and thus
+                the output of the model will be a 2D tensor.
+            - `max` means that global max pooling will
+                be applied.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True, and
+    # Returns
+        A Keras model instance.
+    # Raises
+        ValueError: in case of invalid input shape.
+    """
+
+    if input_tensor is None:
+        img_input = layers.Input(shape=input_shape)
+    else:
+        if not backend.is_keras_tensor(input_tensor):
+            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+    bn_axis = 3
+
+    def round_filters(filters, divisor=depth_divisor):
+        """Round number of filters based on depth multiplier."""
+        filters *= width_coefficient
+        new_filters = max(divisor, int(filters + divisor / 2) // divisor * divisor)
+        # Make sure that round down does not go down by more than 10%.
+        if new_filters < 0.9 * filters:
+            new_filters += divisor
+        return int(new_filters)
+
+    def round_repeats(repeats):
+        """Round number of repeats based on depth multiplier."""
+        return int(math.ceil(depth_coefficient * repeats))
+
+    # Build stem
+    x = img_input
+    x = layers.ZeroPadding2D(padding=correct_pad(backend, x, 3),
+                             name='stem_conv_pad')(x)
+    x = layers.Conv2D(round_filters(32), 3,
+                      strides=2,
+                      padding='valid',
+                      use_bias=False,
+                      kernel_initializer=CONV_KERNEL_INITIALIZER,
+                      name='stem_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name='stem_bn')(x)
+    x = layers.Activation(activation_fn, name='stem_activation')(x)
+
+    # Build blocks
+    from copy import deepcopy
+    blocks_args = deepcopy(blocks_args)
+
+    b = 0
+    blocks = float(sum(args['repeats'] for args in blocks_args))
+    for (i, args) in enumerate(blocks_args):
+        assert args['repeats'] > 0
+        # Update block input and output filters based on depth multiplier.
+        args['filters_in'] = round_filters(args['filters_in'])
+        args['filters_out'] = round_filters(args['filters_out'])
+
+        for j in range(round_repeats(args.pop('repeats'))):
+            # The first block needs to take care of stride and filter size increase.
+            if j > 0:
+                args['strides'] = 1
+                args['filters_in'] = args['filters_out']
+            x = kblock(x, activation_fn, drop_connect_rate * b / blocks,
+                      name='block{}{}_'.format(i + 1, chr(j + 97)), **args,
+                      kType=kType)
+            b += 1
+
+    # Build top
+    x = layers.Conv2D(round_filters(1280), 1,
+                      padding='same',
+                      use_bias=False,
+                      kernel_initializer=CONV_KERNEL_INITIALIZER,
+                      name='top_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, name='top_bn')(x)
+    x = layers.Activation(activation_fn, name='top_activation')(x)
+    if include_top:
+        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+        if dropout_rate > 0:
+            x = layers.Dropout(dropout_rate, name='top_dropout')(x)
+        x = layers.Dense(classes,
+                         activation='softmax',
+                         kernel_initializer=DENSE_KERNEL_INITIALIZER,
+                         name='probs')(x)
+    else:
+        if pooling == 'avg':
+            x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+        elif pooling == 'max':
+            x = layers.GlobalMaxPooling2D(name='max_pool')(x)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = utils.get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+
+    # Create model.
+    model = models.Model(inputs, x, name=model_name)
+
+    return model
+
 
 def EfficientNetB0(include_top=True,
                    input_tensor=None,
@@ -382,6 +663,21 @@ def EfficientNetB0(include_top=True,
                         include_top=include_top,
                         input_tensor=input_tensor, input_shape=input_shape,
                         pooling=pooling, classes=classes,
+                        **kwargs)
+
+def kEfficientNetB0(include_top=True,
+                   input_tensor=None,
+                   input_shape=None,
+                   pooling=None,
+                   classes=1000,
+                   kType=1, 
+                   **kwargs):
+    return EfficientNet(1.0, 1.0, 224, 0.2,
+                        model_name='efficientnet-b0',
+                        include_top=include_top,
+                        input_tensor=input_tensor, input_shape=input_shape,
+                        pooling=pooling, classes=classes,
+                        kType=kType,
                         **kwargs)
 
 
