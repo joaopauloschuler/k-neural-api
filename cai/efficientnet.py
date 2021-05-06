fdfb17d766b9881b3caca33a14cab8c7f3cae657
joaopauloschuler <jp@schulers.com>
2021-05-06 07:00:56 +0200

    Working on EfficientNet

--- cai/efficientnet.py
+++ cai/efficientnet.py
@@ -792,6 +792,103 @@ def kEfficientNetB0(include_top=True,
                         kType=kType,
                         **kwargs)
 
+def kEfficientNetB1(include_top=True,
+                   input_tensor=None,
+                   input_shape=None,
+                   pooling=None,
+                   classes=1000,
+                   **kwargs):
+    return kEfficientNet(1.0, 1.1, 240, 0.2,
+                        model_name='efficientnet-b1',
+                        include_top=include_top,
+                        input_tensor=input_tensor, input_shape=input_shape,
+                        pooling=pooling, classes=classes,
+                        **kwargs)
+
+
+def kEfficientNetB2(include_top=True,
+                   input_tensor=None,
+                   input_shape=None,
+                   pooling=None,
+                   classes=1000,
+                   **kwargs):
+    return kEfficientNet(1.1, 1.2, 260, 0.3,
+                        model_name='efficientnet-b2',
+                        include_top=include_top,
+                        input_tensor=input_tensor, input_shape=input_shape,
+                        pooling=pooling, classes=classes,
+                        **kwargs)
+
+
+def kEfficientNetB3(include_top=True,
+                   input_tensor=None,
+                   input_shape=None,
+                   pooling=None,
+                   classes=1000,
+                   **kwargs):
+    return kEfficientNet(1.2, 1.4, 300, 0.3,
+                        model_name='efficientnet-b3',
+                        include_top=include_top,
+                        input_tensor=input_tensor, input_shape=input_shape,
+                        pooling=pooling, classes=classes,
+                        **kwargs)
+
+
+def kEfficientNetB4(include_top=True,
+                   input_tensor=None,
+                   input_shape=None,
+                   pooling=None,
+                   classes=1000,
+                   **kwargs):
+    return kEfficientNet(1.4, 1.8, 380, 0.4,
+                        model_name='efficientnet-b4',
+                        include_top=include_top,
+                        input_tensor=input_tensor, input_shape=input_shape,
+                        pooling=pooling, classes=classes,
+                        **kwargs)
+
+
+def kEfficientNetB5(include_top=True,
+                   input_tensor=None,
+                   input_shape=None,
+                   pooling=None,
+                   classes=1000,
+                   **kwargs):
+    return kEfficientNet(1.6, 2.2, 456, 0.4,
+                        model_name='efficientnet-b5',
+                        include_top=include_top,
+                        input_tensor=input_tensor, input_shape=input_shape,
+                        pooling=pooling, classes=classes,
+                        **kwargs)
+
+
+def kEfficientNetB6(include_top=True,
+                   input_tensor=None,
+                   input_shape=None,
+                   pooling=None,
+                   classes=1000,
+                   **kwargs):
+    return kEfficientNet(1.8, 2.6, 528, 0.5,
+                        model_name='efficientnet-b6',
+                        include_top=include_top,
+                        input_tensor=input_tensor, input_shape=input_shape,
+                        pooling=pooling, classes=classes,
+                        **kwargs)
+
+
+def kEfficientNetB7(include_top=True,
+                   input_tensor=None,
+                   input_shape=None,
+                   pooling=None,
+                   classes=1000,
+                   **kwargs):
+    return kEfficientNet(2.0, 3.1, 600, 0.5,
+                        model_name='efficientnet-b7',
+                        include_top=include_top,
+                        input_tensor=input_tensor, input_shape=input_shape,
+                        pooling=pooling, classes=classes,
+                        **kwargs)
+   
 
 def preprocess_input(x, data_format=None, **kwargs):
     """Preprocesses a numpy array encoding a batch of images.
