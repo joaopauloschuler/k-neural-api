a2398e818416b24969083e6b27377dcb35a2ff02
joaopauloschuler <jp@schulers.com>
2021-06-29 08:30:50 +0200

    Adding binary channel operations.

--- cai/layers.py
+++ cai/layers.py
@@ -114,6 +114,64 @@ def GlobalAverageMaxPooling2D(previous_layer,  name=None):
       keras.layers.GlobalMaxPooling2D(name=name+'_max')(previous_layer)
     ])
 
+def FitChannelCountTo(last_tensor, next_channel_count, channel_axis=3):
+    prev_layer_channel_count = keras.backend.int_shape(last_tensor)[channel_axis]
+    full_copies = next_channel_count // prev_layer_channel_count
+    extra_channels = next_channel_count % prev_layer_channel_count
+    output_copies = []
+    for copy_cnt in range(full_copies):
+        output_copies.append(last_tensor)
+    if (extra_channels > 0):
+        output_copies.append( CopyChannels(0,extra_channels)(last_tensor) ) 
+    last_tensor = keras.layers.Concatenate(axis=channel_axis)( output_copies )
+    return last_tensor
+
+def EnforceEvenChannelCount(last_tensor, channel_axis=3):
+    prev_layer_channel_count = keras.backend.int_shape(last_tensor)[channel_axis]
+    if (prev_layer_channel_count % 2 > 0):
+        last_tensor = FitChannelCountTo(
+            last_tensor,
+            next_channel_count=prev_layer_channel_count+1,
+            channel_axis=channel_axis)
+    return last_tensor
+
+def BinaryConvLayers(last_tensor, name, shape=(3, 3), conv_count=1, channel_axis=3):
+    last_tensor = EnforceEvenChannelCount(last_tensor)
+    prev_layer_channel_count = keras.backend.int_shape(last_tensor)[channel_axis]
+    for conv_cnt in range(conv_count):
+        input_tensor = last_tensor
+        x1 = keras.layers.Conv2D(prev_layer_channel_count//2, shape, padding='same', activation=None, name=name+"_a_"+str(conv_cnt), groups=prev_layer_channel_count//2)(last_tensor)
+        x2 = keras.layers.Conv2D(prev_layer_channel_count//2, shape, padding='same', activation=None, name=name+"_b_"+str(conv_cnt), groups=prev_layer_channel_count//2)(last_tensor)
+        last_tensor = keras.layers.Concatenate(axis=channel_axis, name=name+"_conc_"+str(conv_cnt))([x1,x2])
+        last_tensor = keras.layers.BatchNormalization(axis=channel_axis, name=name+"_batch_"+str(conv_cnt))(last_tensor)
+        last_tensor = keras.layers.ReLU(name=name+"_relu_"+str(conv_cnt))(last_tensor)
+        last_tensor = keras.layers.add([input_tensor, last_tensor], name=name+'_add'+str(conv_cnt))
+        last_tensor = keras.layers.BatchNormalization(axis=channel_axis)(last_tensor)
+    return last_tensor
+
+def BinaryPointwiseConvLayers(last_tensor, name, conv_count=1, channel_axis=3):
+    return BinaryConvLayers(last_tensor, name, shape=(3, 3), conv_count=conv_count, channel_axis=channel_axis)
+
+def BinaryCompressionLayer(last_tensor, name, channel_axis=3):
+    last_tensor = EnforceEvenChannelCount(last_tensor)
+    prev_layer_channel_count = keras.backend.int_shape(last_tensor)[channel_axis]
+    last_tensor = keras.layers.Conv2D(prev_layer_channel_count//2, (1, 1), padding='same', activation=None, name=name+"_conv", groups=prev_layer_channel_count//2)(last_tensor)
+    last_tensor = keras.layers.BatchNormalization(axis=channel_axis, name=name+"_batch")(last_tensor)
+    last_tensor = keras.layers.ReLU(name=name+"_relu")(last_tensor)
+    return last_tensor
+
+def BinaryCompression(last_tensor, name, target_channel_count, channel_axis=3):
+    prev_layer_channel_count = keras.backend.int_shape(last_tensor)[channel_axis]
+    cnt = 0
+    while (prev_layer_channel_count >= target_channel_count * 2):
+        last_tensor = BinaryCompressionLayer(last_tensor, name=name+'_'+str(cnt), channel_axis=channel_axis)
+        prev_layer_channel_count = keras.backend.int_shape(last_tensor)[channel_axis]
+        cnt = cnt + 1
+    if prev_layer_channel_count > target_channel_count:
+        last_tensor = FitChannelCountTo(last_tensor, next_channel_count=target_channel_count*2, channel_axis=channel_axis)
+        last_tensor = BinaryCompressionLayer(last_tensor, name=name+'_'+str(cnt), channel_axis=channel_axis)
+    return last_tensor
+  
 def GetClasses():
     """
     This function returns CAI layer classes.
