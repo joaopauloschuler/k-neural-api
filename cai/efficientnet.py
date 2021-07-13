5f765ea1d3d8c194fd2ab68b2a8ff972a4137c17
joaopauloschuler <jp@schulers.com>
2021-07-13 08:45:47 +0200

    Coding parallel EfficientNet

--- cai/efficientnet.py
+++ cai/efficientnet.py
@@ -889,7 +889,7 @@ def kEfficientNet(width_coefficient,
     if (len(output_layers)==1):
         x = output_layers[0]
     else:
-        x = keras.layers.Concatenate(axis=bn_axis)([output_layers])
+        x = keras.layers.Concatenate(axis=bn_axis)(output_layers)
 
     # Build top
     #x = layers.Conv2D(round_filters(1280), 1,
